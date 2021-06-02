19f43c4c8b1e514bb085bf645ad163957879d2b1
anzal <anzaldidah@gmail.com>
2021-06-03 02:18:16 +0300

    updated activity

--- app/src/main/java/com/moringaschool/signin/MainActivity.java
+++ app/src/main/java/com/moringaschool/signin/MainActivity.java
@@ -9,11 +9,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button mStartButton;
-
+    private Button mSignButton;
+    private Button mNotesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNotesButton=(Button) findViewById(R.id.notesButton);
+        mSignButton = (Button) findViewById(R.id.signButton);
         mStartButton = (Button) findViewById(R.id.startButton);
         mStartButton.setOnClickListener(new View.OnClickListener() {
 
@@ -24,5 +27,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        mSignButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        mNotesButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,NoteEditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
